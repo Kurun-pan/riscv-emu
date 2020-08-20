be763f189b2339bc0e5c2adc93e7c6f4fdba3b9f
Kurun <38722979+Kurun-pan@users.noreply.github.com>
2020-08-20 21:06:03 +0900

    Implement registers.

--- src/peripherals/uart.rs
+++ src/peripherals/uart.rs
@@ -1,20 +1,40 @@
-// UART 16550
-// http://caro.su/msx/ocm_de1/16550.pdf
+// 16550a UART
+// http://byterunner.com/16550.html
 
 use crate::tty::Tty;
 
-pub struct Uart {
-    rhr: u8, // RO
-    thr: u8, // WO
-    ier: u8, // RW
-    isr: u8, // RO
-    fcr: u8, // WO
-    lcr: u8, // RW
-    mcr: u8, // RW
-    lsr: u8, // RO
-    msr: u8, // RO
-    spr: u8, // RW
+const IER_DATA_READY: u8 = 0x01;
+const IER_THR_EMPTY: u8 = 0x02;
 
+const ISR_INTERRUPT_STATUS_NO_INTERRUPT: u8 = 0x01;
+const ISR_IDENTIFICATION_CODE_DATA_READY: u8 = 0x4;
+const ISR_IDENTIFICATION_CODE_THR_EMPTY: u8 = 0x2;
+
+const LSR_DATA_READY: u8 = 0x01;
+const LSR_THR_EMPTY: u8 = 0x20;
+
+pub struct Uart {
+    /// Receive Hold Register, RO
+    rhr: u8,
+    /// Transmit Hold Register, WO
+    thr: u8,
+    /// Interrupt Enable Register (IER), R/W    
+    ier: u8,
+    /// Interrupt Status Register (ISR), RO
+    isr: u8,
+    /// FIFO Control Register (FCR), WO
+    fcr: u8,
+    /// Line Control Register (LCR), R/W
+    lcr: u8,
+    /// Modem Control Register (MCR), R/W
+    mcr: u8,
+    /// Line Status Register (LSR), RO
+    lsr: u8,
+    /// Modem Status Register (MSR), RO
+    msr: u8,
+    /// ScratchPad Register (SPR), R/W
+    spr: u8,
+    /// Terminal for serial console.
     tty: Box<dyn Tty>,
 }
 
@@ -36,48 +56,36 @@ impl Uart {
     }
 
     pub fn tick(&mut self) {
-        // input
+        // receiver
         if self.rhr == 0 {
             match self.tty.getchar() {
                 0 => {}
                 c => {
                     self.rhr = c;
-
-                    // assert Data Ready interrupt.
-                    self.lsr |= 0x1;
+                    if (self.ier & IER_DATA_READY) > 0 {
+                        self.lsr |= LSR_DATA_READY;
+                    }
                 }
             }
         }
-
-        // output
+        // transmitter
         if self.thr != 0 {
             self.tty.putchar(self.thr);
             self.thr = 0;
-
-            // assert THR Empty interrupt.
-            self.lsr |= 0x20;
+            if (self.ier & IER_THR_EMPTY) > 0 {
+                self.lsr |= LSR_THR_EMPTY;
+            }
         }
-    }
-
-    pub fn is_irq(&mut self) -> bool {
-        if (self.ier & 0x1) > 0 && self.rhr != 0 {
-            // set "Received Data Ready" code.
-            self.isr = self.isr & 0xf0 | 0x4;
-            return true;
-        }
-        if (self.ier & 0x2) > 0 && self.thr == 0 {
-            // set "Transmitter Holding Register Empty" code.
-            self.isr = self.isr & 0xf0 | 0x2;
-            return true;
-        }
-        self.isr = self.isr & 0xf0 | 0x1;
-        false
     }
 
     pub fn read(&mut self, addr: u64) -> u8 {
-        // TODO: Implement here.
         match addr & 0x7 {
-            0 => self.rhr,
+            0 => {
+                let rhr = self.rhr;
+                self.rhr = 0;
+                self.lsr &= !LSR_DATA_READY;
+                rhr
+            }
             1 => self.ier,
             2 => self.isr,
             3 => self.lcr,
@@ -90,17 +98,32 @@ impl Uart {
     }
 
     pub fn write(&mut self, addr: u64, data: u8) {
-        // TODO: Implement here.
         match addr & 0x7 {
-            0 => self.thr = data,
+            0 => {
+                self.thr = data;
+                self.lsr &= !LSR_THR_EMPTY;
+            }
             1 => self.ier = data,
             2 => self.fcr = data,
             3 => self.lcr = data,
             4 => self.mcr = data,
-            5 => self.lsr = data,
-            6 => self.msr = data,
+            5 => {}
+            6 => {}
             7 => self.spr = data,
             _ => panic!(),
         }
+    }
+
+    pub fn is_irq(&mut self) -> bool {
+        if (self.lsr & LSR_DATA_READY) > 0 {
+            self.isr = self.isr & 0xf0 | ISR_IDENTIFICATION_CODE_DATA_READY;
+            return true;
+        }
+        if (self.lsr & LSR_THR_EMPTY) > 0 {
+            self.isr = self.isr & 0xf0 | ISR_IDENTIFICATION_CODE_THR_EMPTY;
+            return true;
+        }
+        self.isr = self.isr & 0xf0 | ISR_INTERRUPT_STATUS_NO_INTERRUPT;
+        false
     }
 }
