1c6b4a17bb092568f06c50f73463ab8d8755ce19
Kurun <38722979+Kurun-pan@users.noreply.github.com>
2020-08-22 16:31:17 +0900

    Format code.

--- src/cpu/trap.rs
+++ src/cpu/trap.rs
@@ -1,32 +1,31 @@
-
 pub struct Trap {
-  pub exception: Exception,
-  pub value: u64
+    pub exception: Exception,
+    pub value: u64,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum Exception {
-  InstructionAddressMisaligned = 0,
-  InstructionAccessFault = 1,
-  IllegalInstruction = 2,
-  Breakpoint = 3,
-  LoadAddressMisaligned = 4,
-  LoadAccessFault = 5,
-  StoreAddressMisaligned = 6,
-  StoreAccessFault = 7,
-  EnvironmentCallFromUMode = 8,
-  EnvironmentCallFromSMode = 9,
-  /* Reserved */
-  EnvironmentCallFromMMode = 11,
-  InstructionPageFault = 12,
-  LoadPageFault = 13,
-  /* Reserved for future standart use */
-  StorePageFault = 15,
+    InstructionAddressMisaligned = 0,
+    InstructionAccessFault = 1,
+    IllegalInstruction = 2,
+    Breakpoint = 3,
+    LoadAddressMisaligned = 4,
+    LoadAccessFault = 5,
+    StoreAddressMisaligned = 6,
+    StoreAccessFault = 7,
+    EnvironmentCallFromUMode = 8,
+    EnvironmentCallFromSMode = 9,
+    /* Reserved */
+    EnvironmentCallFromMMode = 11,
+    InstructionPageFault = 12,
+    LoadPageFault = 13,
+    /* Reserved for future standart use */
+    StorePageFault = 15,
 }
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum Interrupt {
     // Interrupts
     UserSoftware = 0,
