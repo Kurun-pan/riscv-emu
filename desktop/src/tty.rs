f33b5f656f50d9cbc604e76cfd7163474e7662ce
Kurun <38722979+Kurun-pan@users.noreply.github.com>
2021-01-06 22:35:50 +0900

    Ignore graphics commands

--- desktop/src/tty.rs
+++ desktop/src/tty.rs
@@ -8,6 +8,7 @@ use riscv_emu::console::Console;
 pub struct Tty {
     window: Window,
     in_esc_sequences: bool,
+    esc_command_value: u32,
 }
 
 impl Tty {
@@ -21,6 +22,7 @@ impl Tty {
         Tty {
             window: w,
             in_esc_sequences: false,
+            esc_command_value: 0,
         }
     }
 }
@@ -32,18 +34,37 @@ impl Console for Tty {
         // TODO: support ANSI Escape sequences.
         // http://ascii-table.com/ansi-escape-sequences.php
         match c {
-            0x0d => {
+            0x0d/* CR */ => {
                 // TODO: support CR.
                 return;
             }
-            0x1b => {
-                // ESC
+            0x1b/* ESC */ => {
                 self.in_esc_sequences = true;
                 return;
             }
-            0x5b => {
-                // [
+            0x5b/* [ */ => {
                 if self.in_esc_sequences {
+                    return;
+                }
+            }
+            0x3b/* ; */ => {
+                // todo: support arguments of commands
+                if self.in_esc_sequences {
+                    return;
+                }
+            }
+            0x6d/* m */ => {
+                // todo: support graphics commands.
+                if self.in_esc_sequences {
+                    self.in_esc_sequences = false;
+                    self.esc_command_value = 0;
+                    return;
+                }
+            }
+            0x30/* 0 */ | 0x31/* 1 */ | 0x32/* 2 */ | 0x33/* 3 */ | 0x34/* 4 */ |
+            0x35/* 5 */ | 0x36/* 6 */ | 0x37/* 7 */ | 0x38/* 8 */ | 0x39/* 9 */ => {
+                if self.in_esc_sequences {
+                    self.esc_command_value = self.esc_command_value * 10 + (c - 0x30) as u32;
                     return;
                 }
             }
